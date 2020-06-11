599e42c8006db6a0abe57312528414030429ebcf
Tony Rim <duc_thinh_03@yahoo.com>
2020-06-11 00:43:49 +0000

    Automatically backed up by Learn

--- db/migrate/20200611004106_create_dogs.rb
+++ db/migrate/20200611004106_create_dogs.rb
@@ -6,12 +6,15 @@ class CreateDogs < ActiveRecord::Migration[5.2]
   end
   end 
   
+  
   def up 
     create_table :dogs do |t|
       t.string :name 
       t.string :breed
     end 
-  end 
+  end
+  
+  
   
   
 end
